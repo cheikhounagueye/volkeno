049254d7b1f326167c3c163418bfc6b88dc25245
Cheikhouna Gueye <cheikhounagueye4@gmail.com>
2018-01-15 15:21:04 +0000

    first commit

---
+++ app/src/main/java/info/androidhive/floatinglabels/MainActivity.java
@@ -0,0 +1,28 @@
+package info.androidhive.floatinglabels;
+
+import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
